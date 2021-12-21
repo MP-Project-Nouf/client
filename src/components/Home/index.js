21a05e12728e5b990ba9fd1de712fb7df02625f4
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-21 22:02:44 +0300

    edit home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react"
 import Header from "../Header";
 import Image from "./bghome.webp";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import {ArrowRightIcon} from '@chakra-ui/icons';
+
 import './style.css'
 
 import { Button, ButtonGroup,Stack } from "@chakra-ui/react";
 
 function Home() {
+    const navigate = useNavigate();
     const state = useSelector((state) => {
         console.log("state", state);
         return state;
       });
     const [image] = useState(Image);
+
+   
+    const goChallege=()=>{
+        navigate(`/signin`);
+    }
   return (
     <>
       <Header />
@@ -32,6 +41,10 @@ function Home() {
                 </Button>
                 
               </Stack>
+            </div>
+            <div className="startChall" onClick={goChallege}>
+                <h1>إبدأ التحدي</h1>
+                <ArrowRightIcon />
             </div>
           </div>
         </div>
