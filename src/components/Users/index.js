5205ea07888762814dca13f1bfabff9bcd48d313
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-24 20:44:34 +0300

    create search for user

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import image from "./bgUsers.jpg";
 import {  useSelector } from "react-redux";
 import "./style.css";
-import { Avatar} from "@chakra-ui/react";
+import { Avatar,Input} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 function Users() {
@@ -25,16 +25,49 @@ function Users() {
   const goprofile=(id)=>{
     navigate(`/user/${id}`);
   }
+
+  const [searchUser, setSearchUser] = useState([]);
+
+
+  // let productInSearch = [];
+
+  const getSearched = async (e) => {
+    e.preventDefault();
+    const search = await axios.get(`${process.env.REACT_APP_BASIC_URL}/search/${e.target.value}`);
+    // search.data.forEach((element) => {
+    //   console.log("element", element);
+    //   if (
+    //     element.kind.toLowerCase().includes(name) ||
+    //     element.name.toLowerCase().includes(name)
+    //   ) {
+    //     console.log("elementin loop", element);
+    //     productInSearch.push(element);
+    //   }
+    // });
+    // console.log("productInSearch", search.data);
+    setSearchUser(search.data);
+  };
+
+  // useEffect(() => {
+  //   getSearched();
+  // }, [name]);
+
   useEffect(() => {
     getAllUser();
   }, []);
   return (
     <>
       <Header />
+      
+      
       <div className="users">
         <div className="users-container">
           <img className="users-image" src={image} alt="image" />
+          <Input placeholder='ابحث عن اسم المستخدم او الايميل' className="search" onKeyUp={(e)=>{getSearched(e)}}
+        autoFocus/>
+          
           <div className="user-container">
+          
           <div  className="user head">
           <h4>الصورة الشخصية</h4> 
                   <h4>اسم المستخدم</h4>
@@ -42,8 +75,26 @@ function Users() {
                   <h4>المستوى</h4>
                   <h4>عدد النقاط</h4>
                 </div>
-          {users &&
-            users.length &&
+          {(searchUser&&searchUser.length )?
+          searchUser.map((item,i)=>{
+            console.log("hello")
+            return (
+              <div key={i} className="user" onClick={()=>{goprofile(item._id)}}>
+                  <Avatar
+                    name="Dan Abrahmov"
+                    src={item.avatar}
+                    className="line"
+                  />
+                <h4>{item.username}@</h4>
+                <h4>{item.email}</h4>
+                <h4>{item.level}</h4>
+                <h4>{item.point}</h4>
+              </div>
+            );
+
+          }):
+         ( users &&
+            users.length) &&
             users.map((item) => {
               return (
                 <div key={item._id} className="user" onClick={()=>{goprofile(item._id)}}>
